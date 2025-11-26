f8b6092064cad2b28aaca573b237dcd31a2533bd
cyrille <cyrilleferly860@gmail.com>
2025-11-26 14:34:39 +0100

    quizer

--- src/components/quiz.tsx
+++ src/components/quiz.tsx
@@ -121,7 +121,7 @@ const QuizPlay = () => {
 
                         {correctionVisible && (
                             <div className="bg-green-400 border border-gray-700 rounded-xl p-6 space-y-3">
-                                <p className="text-lg font-semibold">
+                                <p className="text-lg font-semibold text-green-600">
                                     {selected === question.answer ? "✔ Bonne réponse !" : "❌ Mauvaise réponse"}
                                 </p>
                                 <p className="text-gray-300">{question.explanation}</p>
